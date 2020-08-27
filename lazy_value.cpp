35d102a3aa5821360b9173847a7a1fd651a2a36e
Niksol15 <44037157+Niksol15@users.noreply.github.com>
2020-08-27 12:54:10 +0300

    Create lazy_value.cpp

---
+++ lazy_value.cpp
@@ -0,0 +1,24 @@
+#include <functional>
+#include <optional>
+
+using namespace std;
+
+template <typename T>
+class LazyValue {
+public:
+    explicit LazyValue(function<T()> init): constructor(move(init)){}
+
+    bool HasValue() const{
+        return optional_value.has_value();
+    }
+    const T& Get() const{
+        if(!optional_value.has_value()){
+            optional_value = constructor();
+        }
+        return optional_value.value();
+    }
+
+private:
+    function<T()> constructor;
+    mutable optional<T> optional_value = nullopt;
+};
